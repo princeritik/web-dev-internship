d323bb65384191e13672f0067f5ce64655b87c0d
PrinceRitik <hritikraj66778@gmail.com>
2026-06-12 20:12:38 +0530

    Added Week 2 Responsive Portfolio Website

---
+++ Assignment2EnhancingYourPortfolioWebsite/script.js
@@ -0,0 +1,36 @@
+const themeBtn = document.getElementById("themeToggle");
+
+themeBtn.addEventListener("click", () => {
+
+    document.body.classList.toggle("dark");
+
+    if(document.body.classList.contains("dark")){
+        themeBtn.textContent = "☀️";
+    }
+    else{
+        themeBtn.textContent = "🌙";
+    }
+
+});
+
+const topBtn = document.getElementById("topBtn");
+
+window.addEventListener("scroll", () => {
+
+    if(window.scrollY > 300){
+        topBtn.style.display = "block";
+    }
+    else{
+        topBtn.style.display = "none";
+    }
+
+});
+
+topBtn.addEventListener("click", () => {
+
+    window.scrollTo({
+        top:0,
+        behavior:"smooth"
+    });
+
+});
